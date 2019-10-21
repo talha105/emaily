81a20df3b7bf73e3f3e48fc0f024ff95a909ee00
unknown <you@example.com>
2019-10-21 11:29:18 -0700

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,6 @@
-// if(process.env.NODE_ENV==='production'){
-//     module.exports=require('./prod')
-// }
-// else{
-//     module.exports=require('./dev')
-// }
-module.exports={
-    googleClientID:'999416888855-mvi0ujas93d7gicfc70aj3sb9iscg4lm.apps.googleusercontent.com',
-    googleClientSecret:'pR7GjVqf51RcYMJLXooz6HMD',
-    mongoURI:'mongodb+srv://emaily-dev:Bluereact12@cluster0-gei5w.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey:'ksfjljf09eurjouhxkjns9374hdijknbs'
+if(process.env.NODE_ENV==='production'){
+    module.exports=require('./prod')
+}
+else{
+    module.exports=require('./dev')
 }
